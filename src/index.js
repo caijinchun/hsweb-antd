cd6f68bfbee72e4b7de9cfdcc6e7004d74308dcb
flybass <flybass@163.com>
2017-05-13 19:58:50 +0800

    'dva教程案例'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
+
 import dva from 'dva';
+import 'babel-polyfill';
+import { browserHistory } from 'dva/router';
+import createLoading from 'dva-loading';
+import { message } from 'antd';
+import './index.html';
 import './index.css';
 
+const ERROR_MSG_DURATION = 3; // 3 秒
+
 // 1. Initialize
- const app = dva({
-     initialState: {
-       products: [
-           { name: 'dva', id: 1 },
-           { name: 'antd', id: 2 },
-         ],
-       },
- });
+const app = dva({
+  history: browserHistory,
+  onError(e) {
+    message.error(e.message, ERROR_MSG_DURATION);
+  },
+});
 
 // 2. Plugins
-// app.use({});
+app.use(createLoading());
 
 // 3. Model
-// app.model(require('./models/example'));
-app.model(require('./models/products'));
+// Moved to router.js
+
 // 4. Router
 app.router(require('./router'));
 
